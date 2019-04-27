57329d40e5452ea913e602e852707d16709d88a7
sigrid <sigrid.c@hotmail.fr>
2019-04-27 23:09:58 +0200

    MapScene creation

---
+++ Scenes/MapScene.js
@@ -0,0 +1,27 @@
+export class MapScene extends Phaser.Scene{
+
+	constructor() {
+
+		super({ key: 'MapScene'});
+
+	}
+
+	preload(){
+	
+		//console.log("MapScene preload");
+	
+	}
+
+	create(){
+	
+		//console.log("MapScene create");
+	
+	}
+
+	update(){
+	
+		//console.log("MapScene update");
+	
+	}
+
+}
