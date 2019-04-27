1d1c09eada1ad7a5c3904d84d729976c8cbd178a
sigrid <sigrid.c@hotmail.fr>
2019-04-27 21:32:19 +0200

    Update Fight working

--- Fight.js
+++ Fight.js
@@ -1,26 +1,95 @@
-import { Player } from './Characters/Player.js';
+import { Character } from './Characters/Character.js';
 
 export class Fight{
 
 	constructor(player, ennemy) {
 			
-		Fight.player = player;
-		Fight.playerAction = null;
+		this.actionsStr = [];
+
+		this.player = player;
+		this.playerAction = null;
 				
-		Fight.ennemy = ennemy;
-		Fight.ennemyAction = null;
+		this.ennemy = ennemy;
+		this.ennemyAction = null;
 		
 	}
 
-	start(){
+	setPlayerAction(action){
+		
+		this.playerAction = action;
 
-		while(Fight.player.getHealth() > 0){
+		this.executePlayerAction();
 
-			console.log("player is alive");
+	}
+		
+	executePlayerAction(){
+
+		if(this.actionsStr.length > 0){
+
+			this.actionsStr = [];
 
 		}
 
-		console.log("player is dead");
+		if(this.playerAction === "attack"){
+
+			this.actionsStr.push("You choosed the action: Attack.");
+
+			this.ennemy.takeDamage(this.player.getAttack());
+
+			this.actionsStr.push("The Enemy take " + this.player.getAttack() + " damages!");
+
+		}
+
+		if(this.playerAction !== null){
+
+			this.playerAction = null;			
+
+		}
+		
+		if(this.ennemy.getHealth() === 0){
+
+			this.actionsStr.push("You Won!");
+
+		}
+
+	}
+
+	setEnnemyAction(){
+
+		this.ennemyAction = "attack";
+		this.executeEnnemyAction();
+
+	}
+
+	executeEnnemyAction(){
+		
+		if(this.actionsStr.length > 0){
+
+			this.actionsStr = [];
+
+		}
+
+		if(this.ennemyAction === "attack"){
+
+			this.actionsStr.push("The Enemy choosed the action: Attack.");
+
+			this.player.takeDamage(this.ennemy.getAttack());
+
+			this.actionsStr.push("You take " + this.ennemy.getAttack() + " damages!");
+
+		}
+
+		if(this.ennemyAction !== null){
+
+			this.ennemyAction = null;
+
+		}
+
+		if(this.player.getHealth() === 0){
+
+			this.actionsStr.push("You Lose!");
+
+		}
 
 	}
 
--- Scenes/FightScene.js
+++ Scenes/FightScene.js
@@ -1,3 +1,4 @@
+import { Character } from '../Characters/Character.js';
 import { Player } from '../Characters/Player.js';
 import { Ennemy1 } from '../Characters/Ennemy1.js';
 import { Fight } from '../Fight.js';
@@ -8,9 +9,35 @@ export class FightScene extends Phaser.Scene{
 
 		super({ key: 'FightScene', actve: true});
 
-		FightScene.player = new Player();
-		FightScene.ennemy = new Ennemy1();
-		FightScene.fight = new Fight(FightScene.player, FightScene.ennemy);
+		this.player = new Player();		
+		this.playerHealthTxt = null;
+
+		this.ennemy = new Ennemy1();		
+		this.ennemyHealthTxt = null;
+
+		FightScene.fight = new Fight(this.player, this.ennemy);
+
+		FightScene.attackMenuText = null;
+
+		this.changeTurnCoolDown = 2000; //ms
+
+		FightScene.isPlayerTurn = null;
+		this.timerPlayerTurn = null;
+
+		this.isEnemyTurn = null;
+		this.timerEnemyTurn = null;
+
+	}
+
+	getPlayer(){
+
+		return this.player;
+
+	}
+
+	getEnnemy(){
+
+		return this.ennemy;
 
 	}
 
@@ -23,14 +50,89 @@ export class FightScene extends Phaser.Scene{
 	create(){
 	
 		//console.log("create FightScene");
+				
+		this.playerHealthTxt = this.add.text(0, 0, 'Player Health:' + this.player.getHealth(), { fontFamily: 'Arial', fontSize: 20, color: '#00ff00' });
 
-		FightScene.fight.start();
+		this.ennemyHealthTxt = this.add.text(400, 0, 'Ennemy Health:' + this.ennemy.getHealth(), { fontFamily: 'Arial', fontSize: 20, color: '#00ff00' });
+
+		FightScene.attackMenuText = this.add.text(0, 550, 'Attack', { fontFamily: 'Arial', fontSize: 20, color: '#00ff00' });
+		FightScene.attackMenuText.setInteractive(new Phaser.Geom.Rectangle(0, 0, FightScene.attackMenuText.width, FightScene.attackMenuText.height), Phaser.Geom.Rectangle.Contains);
+
+		FightScene.attackMenuText.on('pointerover', function(pointer){
+			FightScene.attackMenuText.setAlpha(0.5);			
+		});
+		
+		FightScene.attackMenuText.on('pointerout', function(pointer){
+			FightScene.attackMenuText.setAlpha(1.0);
+		});
+
+		FightScene.attackMenuText.on('pointerdown', function(pointer){		
+			FightScene.fight.setPlayerAction("attack");	
+			FightScene.isPlayerTurn = true;
+		});
 
 	}
 
-	update(){
+	update(time, delta){
 	
 		//console.log("upload FightScene");
+		
+		if(FightScene.isPlayerTurn === true){
+			
+			FightScene.attackMenuText.setVisible(false);
+
+			if(this.timerPlayerTurn === null){
+			
+				this.timerPlayerTurn = 0;
+			
+			}
+
+			this.timerPlayerTurn = this.timerPlayerTurn + delta;
+
+		}
+
+		if(this.timerPlayerTurn !== null && this.timerPlayerTurn >= this.changeTurnCoolDown){
+
+			FightScene.isPlayerTurn = false;
+			this.timerPlayerTurn = 0;
+
+			this.ennemyHealthTxt.text = 'Ennemy Health:' + this.ennemy.getHealth();
+
+			if(this.ennemy.getHealth() > 0){
+
+				FightScene.fight.setEnnemyAction();
+				this.isEnemyTurn = true;
+
+			}
+
+		}
+
+		if(this.isEnemyTurn === true){
+
+			if(this.timerEnemyTurn === null){
+			
+				this.timerEnemyTurn = 0;
+			
+			}
+
+			this.timerEnemyTurn = this.timerEnemyTurn + delta;
+				
+		}
+
+		if(this.timerEnemyTurn !== null && this.timerEnemyTurn >= this.changeTurnCoolDown){
+
+			this.isEnemyTurn = false;
+			this.timerEnemyTurn = 0;
+
+			this.playerHealthTxt.text = 'Player Health:' + this.player.getHealth();
+
+			if(this.player.getHealth() > 0){
+
+				FightScene.attackMenuText.setVisible(true);
+
+			}
+
+		}
 
 	}
 
