d44bb6d957d030c0208f599c1c3800fdf172bcc4
sigrid <sigrid.c@hotmail.fr>
2019-04-27 17:18:17 +0200

    FightScene creation

--- Main.js
+++ Main.js
@@ -1,3 +1,5 @@
+import {FightScene} from './Scenes/FightScene.js';
+
 var config = {
 	type: Phaser.AUTO,
 	width: 800,
@@ -5,29 +7,7 @@ var config = {
 	antialias: false,
 	pixelArt: true,
 	roundPixels: true,
-	scene: {
-		preload: preload,
-		create: create,
-		update: update
-		}
+	scene: [FightScene]
 };
 
 var game = new Phaser.Game(config);
-
-function preload(){
-
-	//console.log("preload");
-
-}
-
-function create(){
-
-	//console.log("create");
-
-}
-
-function update(){
-
-	//console.log("update");
-
-}
---
+++ Scenes/FightScene.js
@@ -0,0 +1,27 @@
+export class FightScene extends Phaser.Scene{
+
+	constructor() {
+
+		super({ key: 'FightScene', actve: true});
+
+	}
+
+	preload(){
+	
+		//console.log("preload FightScene");
+
+	}
+
+	create(){
+	
+		//console.log("create FightScene");
+
+	}
+
+	update(){
+	
+		//console.log("upload FightScene");
+
+	}
+
+}
